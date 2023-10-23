f1cb4dd023501745b27eed28940f2f254f0e5cf9
ramki-chatgen <ramakrishna@chatgen.ai>
2023-10-23 14:40:49 +0000

    Remove Unnecessary dependencies

--- src/modules/openai/actions/getOpenAIClient.ts
+++
@@ -1,8 +0,0 @@
-import OpenAI from 'openai';
-import ENV from '../../../environment';
-
-const openai = new OpenAI({
-	apiKey: ENV.OPENAI_API_KEY, // defaults to process.env["OPENAI_API_KEY"]
-});
-
-export default openai;
